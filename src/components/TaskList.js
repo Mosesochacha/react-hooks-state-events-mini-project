603af2eb84f46928f61984bca9e26ae868c69be6
¨kericho¨ <lvisrono90@gmail.com>
2023-01-13 14:40:04 +0300

    completed assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,24 @@
 import React from "react";
+// import { v4 as uuid } from "uuid";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks, deleteTask , click}) {
+  const tasksList = tasks.map((task, ind) =>
+  <Task
+    key={ind}
+    task={task}
+    text={task.text}
+    category={task.category}
+    click={click}
+  />)
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {/* {tasks.map((task) =>(
+        <Task text={task.text} deleteTask={deleteTask} category={task.category} key={task.text}/>
+      ))} */}
+        {tasksList}
     </div>
   );
 }
